a06d88e67e642fa815751e45967968926b8bcc19
Vinicius Backes <viniciusbackesgoncalves@gmail.com>
2026-08-19 19:29:55 -0300

    Add pagina de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Login } from './auth/login/login';
+export const routes: Routes = [
+    { path: '', component: Login },
+];
