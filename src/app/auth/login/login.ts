3faea49c6423a04b439c4b14d298a496a28b0730
Gabriel Ruvinski <gabriel.ruvinski@ufpr.br>
2026-08-30 18:39:09 -0300

    Primeiro teste da página de login usando Tailwind e cores padronizadas

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
